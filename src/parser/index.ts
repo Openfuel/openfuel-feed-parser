a678dbd473287ca089f3b852b1cf2d97acd74bf1
Divy Srivastava <dj.srivastava23@gmail,com>
2020-05-20 20:52:46 +0530

    feat: well-commented file

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -3,10 +3,13 @@
 // ----------------------------------
 
 /**
-* Parser for Python Weekly.
+* Parser for Newsletters
 **/
 
+// The Python Weekly newsletter parser.
 import PyWeekly from "./pythonWeekly";
+
+// The cheerio module for jquery-like element selctors
 import * as cheerio from "cheerio";
 
 // `keyof any` is short for "string | number | symbol"
@@ -16,16 +19,28 @@ function hasKey<O>(obj: O, key: keyof any): key is keyof O {
   return key in obj
 }
 
+// collection of parser with their incoming email.
 const Parsers = {
   "admin@pycoders.com": PyWeekly,
 };
 
+/**
+* Distributes cheerio instances to parsers.
+* @type function
+* @param {string} email - Email ID of the newsletter
+* @param {any} responseData - Base64 string of data containing HTML
+* @private false
+**/
 export default function Parse(email: string, responseData: any) {
+  // create new Buffer from base64 string.
   var buff = new Buffer(responseData, "base64");
+  // convert Buffer to string.
   var html = buff.toString();
-
+  // create new cheerio instance.
   const $ = cheerio.load(html);
+  // type check for obj key existence
   if (hasKey(Parsers, email)) {
+    // handle over the cheerio instance to the particular newsletter parser.
     Parsers[email]($);
   }
 }
--- src/parser/pythonWeekly.ts
+++ src/parser/pythonWeekly.ts
@@ -1,8 +1,30 @@
-const selectors = {
-  CONTENT_DIV: ".mcnTextContent",
-};
+// ----------------------------------
+// pythonWeekly.ts
+// ----------------------------------
 
+/**
+* Parser for the PythonWeekly newsletter.
+**/
+
+/**
+* Selectors for parsing newsletter
+* @type object
+* @private true
+**/
+const selectors = {
+  // the content div is the parent element for every article.
+  // @selector: class
+  CONTENT_DIV: ".mcnTextContent",
+
+}
+
+/**
+* Selectors for parsing newsletter
+* @type function
+* @param {cheerio.Selector} $ - A cheerio instance
+* @private false
+**/
 export default function parser($: any) {
-  // stuff todo here
+  // TODO: Add parsing logic
   console.log($(selectors.CONTENT_DIV));
 }
